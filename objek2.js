a86e73991c1b6c89dbb14b5eccdddef8eba132c9
Zammavu <zammavu@gmail.com>
2026-07-28 11:35:58 +0700

    Add files via upload

---
+++ objek2.js
@@ -0,0 +1,17 @@
+//Deklarasi objek
+const karakter = {
+    nama: "Nizam",
+    kelas: "XI RPL 1",
+    hobi: "Coding",
+    citaCita: "AI Developer",
+}
+//Dot Notation
+console.log(karakter.nama);
+console.log(karakter.kelas);
+console.log(karakter.hobi);
+console.log(karakter.citaCita);
+//Bracket Notation
+console.log(karakter['nama']);
+console.log(karakter['kelas']);
+console.log(karakter['hobi']);
+console.log(karakter['citaCita']);
