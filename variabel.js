04379d8dfbcd18ba589602d25ce204fec09290fd
Zammavu <zammavu@gmail.com>
2026-07-28 08:14:28 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,16 @@
+let skorPemain = 7;
+const namaPemain = "Nizam";
+const umurPemain = 20;
+let nyawa = 3;
+let level = 99;
+let jago = true
+
+console.log(typeof namaPemain);  // "string"
+console.log(typeof skorPemain);  // "number"
+console.log(typeof umurPemain);  // "number"
+console.log(typeof nyawa);  // "number"
+console.log(typeof level);  // "number"
+console.log(typeof jago);  // "boolean"
+
+skorPemain = 7; // let boleh diubah nilainya
+console.log(`Halo, namaku ${namaPemain}, umurku ${umurPemain}, skorku ${skorPemain}, sisa nyawaku ${nyawa}, levelku ${level}, dan jagoku ${jago}`);
