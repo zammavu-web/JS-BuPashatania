c9fe119fb16a0f227dfee66e3d862fc5638af7f5
Zammavu <zammavu@gmail.com>
2026-07-28 08:05:28 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,14 @@
+//Deklarasi Variabel
+let angka = 150; //number
+const sekolah = "SMK Telkom Malang"; //string
+let kondisi = true; //boolean
+let alamat; //undefined
+let nilai = null; //null
+//output
+console.log(typeof angka);
+console.log( sekolah);
+console.log( kondisi);
+console.log(typeof alamat);
+console.log( nilai);
+//template literal
+console.log(`Hai, nilai saya ${angka} dan saat ini saya bersekolah di ${sekolah}`);
